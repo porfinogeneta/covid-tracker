fff785cb13e215e68adad4f4bcab96b6d5ad4a9b
porfinogeneta <szyman123drum@gmail.com>
2021-05-02 13:44:34 +0200

    deploy

---
+++ precache-manifest.d857cdf3a6072bc26169bcad96dabec7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef66ccd7cced1e5546e9",
+    "url": "/covid-tracker/css/app.ca928689.css"
+  },
+  {
+    "revision": "f3545f75ea6df86052973e3f607bd737",
+    "url": "/covid-tracker/img/death2.f3545f75.svg"
+  },
+  {
+    "revision": "d8fa5dc1c5c7e050f5a8df37f4911a02",
+    "url": "/covid-tracker/img/death3.d8fa5dc1.svg"
+  },
+  {
+    "revision": "e432ebb13663f2fe35e9f1db46e4384c",
+    "url": "/covid-tracker/img/recover.e432ebb1.svg"
+  },
+  {
+    "revision": "c00aaeb3d88f36da8b9e9437d97f7750",
+    "url": "/covid-tracker/img/recovered.c00aaeb3.svg"
+  },
+  {
+    "revision": "2402be2756628034379d808b740ab624",
+    "url": "/covid-tracker/img/virus.2402be27.svg"
+  },
+  {
+    "revision": "e62d3a43aaa5039ac3c22faacb6a70d4",
+    "url": "/covid-tracker/img/virus2.e62d3a43.svg"
+  },
+  {
+    "revision": "03e3ead4d1339a0b253e72e07a89a02b",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "ef66ccd7cced1e5546e9",
+    "url": "/covid-tracker/js/app.fbf2c7d9.js"
+  },
+  {
+    "revision": "d03198ce435710a78fa4",
+    "url": "/covid-tracker/js/chunk-vendors.ae19598a.js"
+  },
+  {
+    "revision": "64c62869a73f01a94274e4dfea392c15",
+    "url": "/covid-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-tracker/robots.txt"
+  }
+]);
